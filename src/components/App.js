3d1af73ce438f3efc4f99530db7148b9718d07a2
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-25 23:53:18 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,67 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
+  };
+  const start = () => {
+    setRenderBall(true);
+    document.addEventListener("keydown", handleKeyDown);
+  };
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 37) {
+      let value = x - 5;
+      setX(value);
+      setBallPosition({
+        left: value + "px",
+        top: y + "px"
+      });
+    } else if (event.keyCode === 38) {
+      let value = y + 5;
+      setY(value);
+      setBallPosition({
+        left: x + "px",
+        top: value + "px"
+      });
+    } else if (event.keyCode === 39) {
+      let value = x + 5;
+      setX(value);
+      setBallPosition({
+        left: value + "px",
+        top: y + "px"
+      });
+    } else if (event.keyCode === 40) {
+      let value = y - 5;
+      setY(value);
+      setBallPosition({
+        left: x + "px",
+        top: value + "px"
+      });
+    }
+  };
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else return start();
+  };
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
+      </button>
+      <button onClick={start} className="start">
+        Start
       </button>
       {renderChoice()}
     </div>
